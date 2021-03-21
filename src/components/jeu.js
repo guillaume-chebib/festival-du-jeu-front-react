f288e0f220dca1d0aa02216551dd08102eb34fda
LaunoisRemy <remy.launois@gmail.com>
2021-03-21 16:11:48 +0100

    Add : modals todelete

--- src/components/jeu.js
+++ src/components/jeu.js
@@ -1,24 +1,84 @@
 import React, { useEffect, useState} from 'react';
 import {useHistory} from "react-router-dom";
-import { DataGrid } from '@material-ui/data-grid';
+
+import {CellParams, DataGrid} from '@material-ui/data-grid';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Switch from '@material-ui/core/Switch';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from "@material-ui/core/Button";
+
 import useStylesTableValueColor from "./table/styles";
+import AlertDialogDelete from "./modals/alert_dialog_delete"
+
+const handleDeleteJeu = () => {
+    console.log("oui")
+};
 
 
 
-//TODO: recuperer tous les jeux et les afficher
-//TODO: formulaire pour ajouter un jeu
+
 const Jeu = () => {
 
     const classes = useStylesTableValueColor();
 
     const history = useHistory();
     const [jeux,setJeux] = useState([])
+    const [open, setOpen] = React.useState(false);
 
 
     const columns = [
         { field: 'id', headerName: 'ID', hide: false },
-        { field : 'titre_jeu', headerName: 'Titre du jeu', width: 400}
-        ]
+        { field : 'titre_jeu', headerName: 'Titre du jeu', flex: 1,type: 'string'},
+        { field : 'min_joueur_jeu', headerName: 'Joueur minimum', flex: 1, type: 'number'},
+        { field : 'max_joueur_jeu', headerName: 'Joueur maximum', flex: 1, type: 'number'},
+        { field : 'age_min_jeu', headerName: 'Age requis', flex: 1, type: 'number'},
+        { field : 'duree_jeu', headerName: 'Furée(en min)', flex: 1, type: 'number'},
+        { field: 'Prototype', headerName: 'Prototype', flex: 1,
+            renderCell: (params) =>
+            {
+                return <Switch
+                    checked={params.row.proto_jeu}
+                    disabled
+                    name="checkedA"
+                    inputProps={{'aria-label': 'secondary checkbox'}}
+                />
+
+            }
+        },
+        { field : 'url_consignes_jeu', headerName: 'Règles du jeu', flex: 1},
+        { field : '', headerName: '', flex: 1,
+            renderCell:(params) =>
+            {
+                const handleClickOpen = () => {
+                    setOpen(true);
+                };
+
+                const handleClose = () => {
+                    setOpen(false);
+                };
+                const handleDelete = () => {
+                    console.log("delete")
+                    setOpen(false);
+                };
+
+                return (
+                    <div>
+                        <IconButton aria-label="delete" onClick={handleClickOpen}>
+                            <DeleteIcon />
+                        </IconButton>,
+                        <AlertDialogDelete message={"Truc"} onClose={handleClose} onDelete={handleDelete} open={open}/>
+                    </div>
+
+                )
+            }
+        },
+
+    ]
 
     function renameKey ( obj, oldKey, newKey ) { //permet de renommer les colonnes
         obj[newKey] = obj[oldKey];
@@ -42,9 +102,6 @@ const Jeu = () => {
     },[]);
 
 
-    useEffect( () =>{
-        console.log(jeux)
-    },[jeux])
 
     return (
         <div style={{paddingTop: '2em'}}>
@@ -60,7 +117,6 @@ const Jeu = () => {
                           className={classes.root}
                           rows={jeux}
                           {...jeux} columns={columns} pageSize={5} />
-
             </div>
         </div>
     )
---
+++ src/components/modals/alert_dialog_delete.js
@@ -0,0 +1,38 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+
+export default function AlertDialogDelete({open,message,onDelete,onClose}) {
+    return (
+        <div>
+
+            <Dialog
+                open={open}
+                onClose={onClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {message}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClose} color="primary">
+                        Non
+                    </Button>
+                    <Button onClick={onDelete}  color="primary" autoFocus>
+                        Oui
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
